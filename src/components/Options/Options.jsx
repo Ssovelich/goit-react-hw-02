129f2be94efc7b4c039e7a6f79a5ed913798ea7c
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-09-28 18:21:33 +0300

    added type for btn

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,20 +3,29 @@ import styles from "./Options.module.css";
 const Options = ({ onUpdateFeedback, totalFeedback, onResetFeedback }) => {
   return (
     <div className={styles.wrap}>
-      <button onClick={() => onUpdateFeedback("good")} className={styles.btn}>
+      <button
+        onClick={() => onUpdateFeedback("good")}
+        type="button"
+        className={styles.btn}
+      >
         Good
       </button>
       <button
         onClick={() => onUpdateFeedback("neutral")}
+        type="button"
         className={styles.btn}
       >
         Neutral
       </button>
-      <button onClick={() => onUpdateFeedback("bad")} className={styles.btn}>
+      <button
+        onClick={() => onUpdateFeedback("bad")}
+        type="button"
+        className={styles.btn}
+      >
         Bad
       </button>
       {totalFeedback > 0 && (
-        <button onClick={onResetFeedback} className={styles.btn}>
+        <button onClick={onResetFeedback} type="button" className={styles.btn}>
           Reset
         </button>
       )}
